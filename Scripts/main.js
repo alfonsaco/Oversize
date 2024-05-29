11b3f6b3b22d7199426e13cde7d3cebd5d87edef
alfonsaco <alfonso.rincondev@gmail.com>
2024-05-29 18:12:04 +0200

    Se añade el marquee con sus estilos (beta) y se añade el nav con sus primeros estilos y su animación al pulsar el botón de las 3 rayas

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,7 +1,16 @@
+// Función para desplegar el buscador
 const botonBuscador=document.getElementById('icono-buscar');
 const buscador=document.getElementById('buscador-desplegar');
 
-
 botonBuscador.addEventListener("click", function() {
     buscador.classList.toggle("mover-buscador");
+});
+
+
+// Función para desplegar el nav
+const rayas=document.getElementById("header-barritas");
+const nav=document.getElementById("nav-desplegable");
+
+rayas.addEventListener("click", function() {
+    nav.classList.toggle("mostrar-nav");
 });
