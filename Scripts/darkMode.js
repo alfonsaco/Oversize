49f73baae9305f2e70fc856360a63d990529d5a8
alfonsaco <alfonso.rincondev@gmail.com>
2024-06-24 13:29:38 +0200

    Se agrega el botón para cambiar el tema

---
+++ Scripts/darkMode.js
@@ -0,0 +1,18 @@
+const texto=document.querySelector(".cambiar-tema p");
+const boton=document.querySelector(".boton-cambiar-tema");
+const bola=document.querySelector(".boton-cambiar-tema > div");
+
+let valor=true;
+
+boton.addEventListener("click", function() {
+    bola.classList.toggle("mover-bola");
+    boton.classList.toggle("cambiar-color");
+    if(valor==true) {
+        texto.textContent="Modo claro";
+        valor=false;
+    } else {
+        texto.textContent="Modo oscuro";
+        valor=true;
+    }
+
+});
