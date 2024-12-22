7dc9f2bbca5f00e9510948ce33ca08f8b842a507
alfonsaco <alfonso.rincondev@gmail.com>
2024-12-23 00:05:35 +0100

    Función para mostrar los productos según el tipo terminada

--- Scripts/agregarProductos.js
+++ Scripts/agregarProductos.js
@@ -1,12 +1,16 @@
-window.addEventListener("DOMContentLoaded", function() {
+window.addEventListener("DOMContentLoaded", function () {
     const contenedorRopa=document.querySelector(".contenedor-productos");
+    // Variable para coger el tipo de producto
+    const tipoProducto=contenedorRopa.getAttribute("data-tipo");
 
     contenedorRopa.innerHTML="";
-    
+
     fetch("../Imágenes/Ropa/ropa.json").then(response => response.json()).then(data => {
-        data.forEach(producto => {
-            if(producto.carrusel) {
-                contenido=`<div class="producto">
+
+        // Filtrar los productos, y seleccionar el producto necesario para cada página
+        const tipoDeProducto=data.filter(producto => producto.tipo == tipoProducto);
+        tipoDeProducto.forEach(producto => {        
+            contenido=`<div class="producto">
                     <a href="#">
                         <img src="${producto.ruta}" alt="">
                         <h4>${producto.nombre}</h4>
@@ -14,8 +18,7 @@ window.addEventListener("DOMContentLoaded", function() {
                         <p>${producto.precio}€</p>
                     <a>
                 </div>`;
-                contenedorRopa.innerHTML+=contenido;
-            }
+            contenedorRopa.innerHTML+=contenido;
         });
     }).catch(error => console.error("No se pudieron cargar los productos. ERROR: "+error));
 
