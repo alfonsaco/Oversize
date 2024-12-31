63f4509a153bb156102410e52f86157938717a10
alfonsaco <alfonso.rincondev@gmail.com>
2024-12-31 02:35:01 +0100

    Mejora del Header y del efecto del Nav

--- Scripts/main.js
+++ Scripts/main.js
@@ -11,7 +11,16 @@ botonBuscador.addEventListener("click", function(e) {
 // Función para desplegar el nav
 const rayas=document.getElementById("header-barritas");
 const nav=document.getElementById("nav-desplegable");
+const fondoBlur=document.getElementById("efecto-oscurecer-nav");
 
 rayas.addEventListener("click", function() {
     nav.classList.toggle("mostrar-nav");
+
+    if(fondoBlur.style.visibility == 'visible') {
+        fondoBlur.style.visibility='hidden';
+        fondoBlur.style.opacity='0';
+    } else {
+        fondoBlur.style.visibility='visible';
+        fondoBlur.style.opacity='1';
+    }
 });
